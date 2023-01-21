e2596469077db46780eb0942c50d43c3879d2685
Caio Pivetta <caiopivetta6@gmail.com>
2023-01-21 11:34:11 +0100

    project created

---
+++ src/main/java/com/caiopivetta6/UniversityActivityProjectApplication.java
@@ -0,0 +1,13 @@
+package com.caiopivetta6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityActivityProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversityActivityProjectApplication.class, args);
+	}
+
+}
