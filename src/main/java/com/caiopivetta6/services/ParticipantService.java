ec1adcc3b57723416cfdfb524e9a7807c70ae9c3
Caio Pivetta <caiopivetta6@gmail.com>
2023-01-30 17:55:44 +0100

    Participant endpoint, Service

--- src/main/java/com/caiopivetta6/services/ParticipantService.java
+++ src/main/java/com/caiopivetta6/services/ParticipantService.java
@@ -3,11 +3,17 @@ package com.caiopivetta6.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.caiopivetta6.domain.Participant;
 import com.caiopivetta6.repositories.ParticipantRepository;
 import com.caiopivetta6.resources.exceptions.ObjectNotFoundException;
+import com.caiopivetta6.services.exceptions.DatabaseException;
+import com.caiopivetta6.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class ParticipantService {
@@ -20,6 +26,33 @@ public class ParticipantService {
 		
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Object not found! Id: " + id + ", Tipo: " + Participant.class.getName()));
+	}
+	
+	public void delete(Integer id) {
+		try {
+		repository.deleteById(id);
+		}catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+	}
+	
+	public Participant update(Integer id, Participant obj) {
+		try {
+		Participant entity = repository.getReferenceById(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+		}catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+		
+	}
+	
+	private void updateData(Participant entity, Participant obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		//entity.setPhone(obj.getPhone());
 	}
 	
 }
